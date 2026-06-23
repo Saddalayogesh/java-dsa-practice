9e6209a4b4a640fd034e5e7a31fa4d7b0fd0eba5
saddala yogesh <saddalayogesh@gmail.com>
2026-06-23 23:48:53 +0530

    Added SecondLargestElement program

---
+++ src/com/java/dsa/array/SecondLargestElement.java
@@ -0,0 +1,26 @@
+package com.java.dsa.array;
+
+public class SecondLargestElement {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 50, 30, 20, 40};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Second largest element does not exist.");
+        } else {
+            System.out.println("Second largest element: " + secondLargest);
+        }
+    }
+}
