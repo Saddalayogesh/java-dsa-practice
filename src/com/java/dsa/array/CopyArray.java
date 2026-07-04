8a8eac8c252069f101f0b75661e82075857a68f1
saddala yogesh <saddalayogesh@gmail.com>
2026-07-04 23:21:22 +0530

    add: public static void main(String[] args) {

---
+++ src/com/java/dsa/array/CopyArray.java
@@ -0,0 +1,18 @@
+package com.java.dsa.array;
+
+public class CopyArray {
+
+    public static void main(String[] args) {
+        int[] original = {10, 20, 30, 40, 50};
+        int[] copy = new int[original.length];
+
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i];
+        }
+
+        System.out.print("Copied Array: ");
+        for (int num : copy) {
+            System.out.print(num + " ");
+        }
+    }
+}
