fbc6af324fd965c87b36381752656ebcb6f15f1a
saddala yogesh <saddalayogesh@gmail.com>
2026-07-07 23:49:32 +0530

    add:FrequencyOfElement

---
+++ src/com/java/dsa/array/MoveZerosToEnd.java
@@ -0,0 +1,28 @@
+package com.java.dsa.array;
+
+public class MoveZerosToEnd {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, 2, 0, 3, 0, 4};
+
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.print("Array after moving zeros: ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
