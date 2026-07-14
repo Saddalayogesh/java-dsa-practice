e70b7b88731d2b9f7a2699bb76310183ddac92a6
saddala yogesh <saddalayogesh@gmail.com>
2026-07-14 23:43:02 +0530

    add : CountWords

---
+++ src/com/java/dsa/string/CountWords.java
@@ -0,0 +1,23 @@
+package com.java.dsa.string;
+
+import java.util.Scanner;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine().trim();
+
+        if (str.isEmpty()) {
+            System.out.println("Number of words: 0");
+        } else {
+            String[] words = str.split("\\s+");
+            System.out.println("Number of words: " + words.length);
+        }
+
+        sc.close();
+    }
+}
