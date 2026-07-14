f56f2285719aecc0d345cd88bf56df7fee2f3d0d
saddala yogesh <saddalayogesh@gmail.com>
2026-07-14 23:44:02 +0530

    add : AnagramString

---
+++ src/com/java/dsa/string/AnagramString.java
@@ -0,0 +1,37 @@
+package com.java.dsa.string;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine().replaceAll("\\s", "").toLowerCase();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine().replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Not Anagrams");
+        } else {
+
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            if (Arrays.equals(arr1, arr2)) {
+                System.out.println("Anagrams");
+            } else {
+                System.out.println("Not Anagrams");
+            }
+        }
+
+        sc.close();
+    }
+}
