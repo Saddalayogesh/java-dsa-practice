be2eecd61bbbff3607433894d83b190c59e6deb3
saddala yogesh <saddalayogesh@gmail.com>
2026-07-15 23:34:38 +0530

    add : RemoveDuplicateCharacters

---
+++ src/com/java/dsa/string/FirstNonRepeatingCharacter.java
@@ -0,0 +1,19 @@
+package com.java.dsa.string;
+
+public class FirstNonRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        String str = "swiss";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (str.indexOf(ch) == str.lastIndexOf(ch)) {
+                System.out.println("First Non-Repeating Character: " + ch);
+                break;
+            }
+        }
+    }
+}
