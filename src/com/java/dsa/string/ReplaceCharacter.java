a5702f9eb2c420a0046d8ddbc912107020d2c58a
saddala yogesh <saddalayogesh@gmail.com>
2026-07-19 23:27:15 +0530

    add : ReplaceCharacter

---
+++ src/com/java/dsa/string/ReplaceCharacter.java
@@ -0,0 +1,14 @@
+package com.java.dsa.string;
+
+public class ReplaceCharacter {
+
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        String result = str.replace('a', '@');
+
+        System.out.println("Original String : " + str);
+        System.out.println("Modified String : " + result);
+    }
+}
